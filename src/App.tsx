07013802937e16d31c3eb81e18507a7c797c17c0
Catalin-Teodor <catalin@itsatopic.com>
2025-11-07 15:53:07 +0200

    Reparat eroarea cu pagina albă și refăcut API

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import Header from './components/Header';
 import Hero from './components/Hero';
 import WhyChooseUs from './components/WhyChooseUs';
 import HowItWorks from './components/HowItWorks';
-import Testimonials from './components/Testimonials';
+import Testimonials from './components/Testimonials.tsx';
 import Menu from './components/Menu';
 import OrderForm from './components/OrderForm';
 import Footer from './components/Footer';
